1a77de300ec62881bd75c0c200b5fbfc76aa2933
pooja_solaikannu <pooja.solaikannu@gmail.com>
2025-03-05 19:36:29 +0530

    improved InputBox component with Labels

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -3,11 +3,13 @@ import "./InputBox.css"
 interface IInputBoxProp {
     placeholderValue: string
     inputType: string
+    lableName: string
 }
 
 export default function InputBox(props: IInputBoxProp){
     return(
         <div>
+            <div className="font-semibold mb-2">{props.lableName}</div>
             <input className="border-2 border-gray-500 py-2 px-2 rounded" placeholder={props.placeholderValue} type={props.inputType}></input>
         </div>
     )
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -7,9 +7,9 @@ export function SignIn() {
     }
     return(
         <div className="m-4 flex flex-col items-center justify-center gap-4 bg-gray-200">
-            <InputBox placeholderValue="John" inputType="text"/>
-            <InputBox placeholderValue="Doe" inputType="text"/>
-            <InputBox placeholderValue="password" inputType="text"/>
+            <InputBox lableName="First Name" placeholderValue="John" inputType="text"/>
+            <InputBox lableName="Last Name" placeholderValue="Doe" inputType="text"/>
+            <InputBox lableName="Email"placeholderValue="abc@gmail.com" inputType="text"/>
 
             <Button buttonName="Sign In" buttonColor="black" onClick={signinHandler}/>
         </div>
